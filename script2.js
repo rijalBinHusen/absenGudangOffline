98654c48442199d46e963f5c867aaae6702a6f30
rijalBinHusen <jeryzal333@gmail.com>
2021-07-11 16:00:11 +0700

    success crud indexeddb divisi component

--- script2.js
+++ script2.js
@@ -27,12 +27,16 @@ new Vue({
 			//the counter +1
 			this.deData.lastId[this.currentTab.toLowerCase()]++
 			let id = this.deData.lastId[this.currentTab.toLowerCase()]
+
 			//add to data id
 			dat['id_'+this.currentTab.toLowerCase()] = id
+
 			//insert to indexeddb
 			db.collection(this.currentTab.toLowerCase()).add(dat)
+
             //push to front end data
 			this.deData[this.currentTab.toLowerCase()].push(dat) //masukkan data
+
 			// update the counter
 			this.reWrite('data_absen', {absen: 'lastId'}, {absen: 'lastId', value: this.deData.lastId})
 
@@ -47,10 +51,18 @@ new Vue({
 		update(ev) {
 			//{store:'divisi', id: {id_divisi: this.datanya.id_divisi}, val:{divisi: this.deData}
 			db.collection(ev.store).doc(ev.id).update(ev.val)
-			
+
+			//update front end data
+			let index = cariIndex( this.deData[this.currentTab.toLowerCase()], 
+			{'equalTo': ['id_'+this.currentTab.toLowerCase(), 
+			this.datanyaForm['id_'+this.currentTab.toLowerCase()] ]}
+			)
+
+			this.deData[this.currentTab.toLowerCase()].splice(index, 1)  //hapus
+			this.deData[this.currentTab.toLowerCase()].splice(index, 0, Object.assign({}, ev.id, ev.val)) //sisipkan
+
 			//close modal
             this.modalChange()
-			// this.getDataNew()
         },
 		reWrite(store, id, val) {
 			//(nameStore, {id: idData}, {key: 'new value'})
@@ -105,10 +117,19 @@ new Vue({
 	created () {
 		this.getDivisi()
 		this.getMasterCounter()
-		// this.deData.divisi = crud('read', 'divisi')
-		// this.deData.bagian = crud('read', 'bagian')
-		// this.deData.level = crud('read', 'level')
-		// this.deData.karyawan = crud('read', 'karyawan')
-		// this.deData.absen = crud('read', 'absen')
 	}
   });
+
+
+function cariIndex (obj, criteria) {
+	//obj = [ {"id": 1, "item1": "item content item content"} ]
+	//cireteria = { "equalTo": ["ObjectKey", "key to find"] }
+  
+	let result = ''
+	if(criteria.equalTo) {
+		obj.filter((val, index) => {
+			val[criteria.equalTo[0]] == criteria.equalTo[1] ? result = index : ''
+		})
+	}
+	return result
+  }
